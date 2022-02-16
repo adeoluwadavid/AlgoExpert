dde721185b84cd59654a3da09acc55eaa0bdde23
David Adewole <adeoluwadavid@gmail.com>
2022-02-16 09:05:09 +0100

    algo-ds

---
+++ typescript/palindrome_check.ts
@@ -0,0 +1,12 @@
+/**
+ * Write a function that takes in a non-empty string and that returns a boolean representing whether the string is a palindrome
+ * 
+ * A Palindrome is defined as a string that's written the same forward and backward. Note that single-charactrer strings are palindromes
+ */
+
+function isPalindrome(string: string){
+    string = string.replace(/\W/g,'').toLowerCase();
+    let result = string === string.split('').reverse().join('')
+   return result
+}
+isPalindrome('abcdcba')
